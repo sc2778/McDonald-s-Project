06fd74bfbc4d683b8c6288977e0a47131ff94037
sc2778 <sc2778@cornell.edu>
2017-04-29 13:16:25 -0400

    upload project

---
+++ js/script.js
@@ -0,0 +1,62 @@
+(function($) {
+  "use strict";
+
+
+
+$(document).ready(function(){
+	
+	
+if (!(/android|blackberry|windows phone|iphone|ipod/i).test(navigator.userAgent.toLowerCase())) {
+
+	var header = $('.mainHeader'),
+		pos = header.offset();
+
+		$(window).scroll(function(){
+			if($(this).scrollTop() > pos.top+20 && header.hasClass('default')){
+				header.fadeOut('fast', function(){
+					$(this).removeClass('default').addClass('switchedHeader').slideDown(200);
+				});
+			} else if($(this).scrollTop() <= pos.top+20 && header.hasClass('switchedHeader')){
+				header.slideUp(200, function(){
+					$(this).removeClass('switchedHeader').addClass('default').fadeIn(200);
+				});
+			}
+	});
+	
+}
+	
+	
+
+$('a.scroll').smoothScroll({
+        speed: 800
+        
+});
+
+$('button.scroll').smoothScroll({
+	top:800,
+	left:0
+});
+
+
+
+
+$(".itemDesc").css({ opacity: 0 });
+
+			
+	
+$('.itemDesc').hover( function(){ 
+	$(this).stop().animate({ opacity: 1 }, 'slow');
+}, function(){ 
+	$(this).stop().animate({ opacity: 0 }, 'slow'); 
+});
+
+		$('.itemDesc').hover(function () {
+					    var itemDesc1 = $(this).find('.itemDesc1');
+					    var offset = ($(this).height() / 2) - (itemDesc1.height() / 2);
+					    $(this).find('.itemDescInner').css('padding-top', offset -30);
+					});
+
+
+});
+
+})(jQuery);
